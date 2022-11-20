17a402de887bcabc116840d87e0e327223c158f2
Nathan Faucett <nathanfaucett@gmail.com>
2022-11-20 15:31:48 -0500

    fix link bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,6 @@
 
 <ul>
 	<li><a href={`${base}/default`}>Default</a></li>
-	<li><a href={`${base}/editor`}>Editor</a></li>
 	<li><a href={`${base}/huge-document`}>Huge Document</a></li>
 	<li><a href={`${base}/plugins`}>Plugins</a></li>
 </ul>
