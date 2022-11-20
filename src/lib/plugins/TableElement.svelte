28968729ea7ee58792a73d1f1b0fc6aac8064b44
Nathan Faucett <nathanfaucett@gmail.com>
2022-11-20 15:56:19 -0500

    allow tabs in tables

--- src/lib/plugins/TableElement.svelte
+++ src/lib/plugins/TableElement.svelte
@@ -1,7 +1,7 @@
 <svelte:options immutable />
 
 <script lang="ts" context="module">
-	import { Transforms, Range, Editor, Point, type NodeMatch, type BaseRange } from 'slate';
+	import { Transforms, Range, Editor, Node } from 'slate';
 	import type { IElement } from './Element.svelte';
 	import type { IElementProps } from '$lib/components/InternalElement.svelte';
 
@@ -25,12 +25,12 @@
 					match: isTableDataElement as any
 				});
 				if (dataMatch) {
-					const [_, path] = dataMatch;
+					const [node, path] = dataMatch;
 					if (
 						path[0] === editor.selection.focus.path[0] &&
 						path[1] === editor.selection.focus.path[1] &&
 						editor.selection.focus.path[2] === 0 &&
-						editor.selection.focus.offset === 0
+						Editor.isEmpty(editor, node as IElement)
 					) {
 						Transforms.delete(editor, {
 							at: [path[0], path[1]]
@@ -41,8 +41,22 @@
 				const [headerMatch] = Editor.nodes(editor, {
 					match: isTableHeaderElement as any
 				});
-				if (!!headerMatch && Editor.isEmpty(editor, headerMatch[0] as IElement)) {
-					return;
+				if (headerMatch) {
+					const [node] = headerMatch;
+					if (Editor.isEmpty(editor, node as IElement)) {
+						const [tableMatch] = Editor.nodes(editor, {
+							match: isTableElement as any
+						});
+						const [node, [index]] = tableMatch;
+						const table = node as ITableElement;
+						for (let i = table.children.length - 1; i >= 0; i--) {
+							const row = table.children[i];
+							Transforms.delete(editor, {
+								at: [index, i, row.children.length - 1]
+							});
+						}
+						return;
+					}
 				}
 			}
 			deleteBackward(...args);
@@ -109,57 +123,76 @@
 	createContext(ELEMENT_CONTEXT_KEY, TableRowElement);
 
 	addEventListener('onKeyDown', (e) => {
-		if (editor.selection && e.key === 'Enter') {
-			const [match] = Editor.nodes(editor, {
-				match: isTableElement as any
-			});
-			if (match) {
-				e.preventDefault();
-				e.stopPropagation();
-				const [node, [index]] = match;
-				const table = node as ITableElement;
-				if (e.shiftKey) {
-					const [headerMatch] = Editor.nodes(editor, {
-						match: isTableHeaderElement as any
-					});
-					if (headerMatch) {
-						table.children.forEach((row, rowIndex) => {
+		if (editor.selection) {
+			if (e.key === 'Enter') {
+				const [match] = Editor.nodes(editor, {
+					match: isTableElement as any
+				});
+				if (match) {
+					e.preventDefault();
+					e.stopPropagation();
+					const [node, [index]] = match;
+					const table = node as ITableElement;
+					if (e.shiftKey) {
+						const [headerMatch] = Editor.nodes(editor, {
+							match: isTableHeaderElement as any
+						});
+						if (headerMatch) {
+							table.children.forEach((row, rowIndex) => {
+								Transforms.insertNodes(
+									editor,
+									{
+										type: rowIndex !== 0 ? TABLE_DATA_TYPE : TABLE_HEADER_TYPE,
+										children: [{ text: '' }]
+									} as any,
+									{
+										at: [index, rowIndex, row.children.length]
+									}
+								);
+							});
+							Transforms.select(editor, [index, 0, table.children[0].children.length]);
+						} else {
+							const at = [index + 1];
 							Transforms.insertNodes(
 								editor,
+								{ type: PARAGRAPH_TYPE, children: [{ text: '' }] } as any,
 								{
-									type: rowIndex !== 0 ? TABLE_DATA_TYPE : TABLE_HEADER_TYPE,
-									children: [{ text: '' }]
-								} as any,
-								{
-									at: [index, rowIndex, row.children.length]
+									at
 								}
 							);
-						});
-						Transforms.select(editor, [index, 0, table.children[0].children.length]);
+							Transforms.select(editor, at);
+						}
 					} else {
-						const at = [index + 1];
+						const at = [index, editor.selection.focus.path[1] + 1];
 						Transforms.insertNodes(
 							editor,
-							{ type: PARAGRAPH_TYPE, children: [{ text: '' }] } as any,
+							{
+								type: TABLE_ROW_TYPE,
+								children: createNRows(table.children[0].children.length)
+							} as any,
 							{
 								at
 							}
 						);
 						Transforms.select(editor, at);
 					}
-				} else {
-					const at = [index, editor.selection.focus.path[1] + 1];
-					Transforms.insertNodes(
-						editor,
-						{
-							type: TABLE_ROW_TYPE,
-							children: createNRows(table.children[0].children.length)
-						} as any,
-						{
-							at
-						}
-					);
-					Transforms.select(editor, at);
+				}
+			} else if (e.key === 'Tab') {
+				const [match] = Editor.nodes(editor, {
+					match: (e) => isTableDataElement(e as IElement) || isTableHeaderElement(e as IElement)
+				});
+				if (match) {
+					e.preventDefault();
+					e.stopPropagation();
+					const [_node, path] = match;
+					const [_rowNode, rowPath] = Editor.parent(editor, path);
+					const [tableNode, _tablePath] = Editor.parent(editor, rowPath);
+					const table = tableNode as ITableElement;
+					const rows = table.children.length;
+					const columns = table.children[0].children.length;
+					const nextColumnIndex = (path[2] + 1) % columns;
+					const nextRowIndex = (path[1] + (nextColumnIndex === 0 ? 1 : 0)) % rows;
+					Transforms.select(editor, [path[0], nextRowIndex, nextColumnIndex]);
 				}
 			}
 		}
