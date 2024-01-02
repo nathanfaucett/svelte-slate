415759984c982f7ac638672b63df0f72a95802a9
nathanfaucett <nathanfaucett@users.noreply.github.com>
2024-01-02 12:34:41 +0000

    deploy: 8fc95474c10e6969e1102cd4703df4c62dafa262

--- internal/immutable/chunks/paths.0965bb69.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1bje3au)==null?void 0:s.base)??"/svelte-slate";var e;const t=((e=globalThis.__sveltekit_1bje3au)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ internal/immutable/chunks/paths.CppdczBK.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1bcq636?.base??"/svelte-slate",e=globalThis.__sveltekit_1bcq636?.assets??s;export{e as a,s as b};
